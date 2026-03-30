27bba1742be0bfd06bd73041a6da10866bdb6b50
danghuyenvu <danghuyenvu516@gmail.com>
2026-03-30 16:36:43 +0700

    skeleton

---
+++ game.py
@@ -0,0 +1,18 @@
+import pygame
+
+from pygame.locals import *
+from settings import * 
+
+class Game():
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode(WINDOW_RESOLUTION)
+
+    def play(self):
+        pass
+
+    def draw(self):
+        pass
+
+    def update(self):
+        pass
---
+++ main.py
@@ -0,0 +1,8 @@
+from game import *
+
+def main():
+    game = Game()
+    game.play()
+
+if __name__ == "__main__":
+    main()
